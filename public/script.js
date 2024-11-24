abde96970a127031baef1d019df0d613a436f449
frnono <fr@nono.my>
2024-11-24 01:59:04 -0400

    v1.4.1

--- public/script.js
+++ public/script.js
@@ -15,7 +15,7 @@ function formatSizeUnits(bytes) {
     const k = 1024;
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+    return (bytes / Math.pow(k, i)).toFixed(2) + ' ' + sizes[i];
 }
 
 function formatTime(date) {
